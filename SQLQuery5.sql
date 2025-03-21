d0f32ff7c6c20fd02f95addee0f9a57fcc68693f
madavan-sedu <122661735+madavan-sedu@users.noreply.github.com>
2025-03-21 10:10:09 +0530

    Delete SQLQuery5.sql

--- SQLQuery5.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO SM (Name, DateOfBirth, Address)  -- Correct column names without quotes
-VALUES ('SEDU', '2003-08-14', 'NO:1/241 EALS STREET');  -- Correct date format (YYYY-MM-DD)
-
-
-
-
